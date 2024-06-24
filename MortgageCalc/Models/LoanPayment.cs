b8935a6bb7faecd1e7302227e52831229e824b3a
Chris Herzig <81333676+ScientistZero@users.noreply.github.com>
2024-06-24 10:55:20 -0400

    Update LoanPayment.cs to show correct currency symbol.

--- MortgageCalc/Models/LoanPayment.cs
+++ MortgageCalc/Models/LoanPayment.cs
@@ -5,9 +5,9 @@ namespace MortgageCalc.Models
     public class LoanPayment
     {
         public int Month { get; set; }
-        
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = true)]
         public decimal Payment { get; set; }
-        
+        [DisplayFormat(DataFormatString = "{0:C2}")]
         public decimal MonthlyPrincipal { get; set; }
         public decimal MonthlyInterest { get; set; }
         public decimal TotalInterest { get; set; }
